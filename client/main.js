31167dcb8e9c939b22de3d6eeb260e375f28af7d
Theuy Limpanont <info@theuy.nl>
2016-04-25 15:24:35 +0200

    add jquery plugin theme switcher

--- client/main.js
+++ client/main.js
@@ -1,3 +1,13 @@
+import jQuery from 'jquery';
+
 if(Meteor.isClient) {
-    
+    jQuery(function() {
+        jQuery('#themeSwitcher').themeSwitcher({
+            themes: [
+                { href: 'themes/darkly/style.css', disabled: true },
+                { href: 'themes/flatly/style.css', disabled: true },
+                { href: 'themes/default/style.css', disabled: false }
+            ]
+        });
+    });
 }
--- client/themeSwitcher.js
+++ client/themeSwitcher.js
@@ -1,28 +1,65 @@
-import { Template } from 'meteor/templating';
+import jQuery from 'jquery';
 
-if(Meteor.isClient) {
-    Template.themeSwitcher.onCreated(function () {
-        this.themes = new ReactiveVar([
-            { href: 'themes/darkly/style.css', disabled: true },
-            { href: 'themes/flatly/style.css', disabled: true },
-            { href: 'themes/default/style.css', disabled: false }
-        ]);
-    });
+(function ($, window, document, undefined) {
 
-    Template.themeSwitcher.events({
-        'change select' : function(event, template) {
-            var themes = template.themes.get();
-            themes.forEach(function(theme) {
-                theme.disabled = true;
-            });
-            var selectedTheme = _.findWhere(themes, { href: event.target.value });
-            selectedTheme.disabled = false;
-            template.themes.set(themes);
+    var pluginName = "themeSwitcher",
+        defaults = {
+            propertyName: "value"
+        };
+
+    function Plugin(element, options) {
+        this.element = element;
+
+        this.options = $.extend({}, defaults, options);
+
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    Plugin.prototype = {
+
+        init: function () {
+
+            this.options.themes.forEach(function (theme) {
+                var $option, $link;
+
+                if (theme.disabled) {
+                    $option = '<option value="' + theme.href + '">' + theme.href + '</option>';
+                    $link = '<link class="appTheme" rel="stylesheet" href="' + theme.href + '" disabled="disabled">';
+                } else {
+                    $option = '<option value="' + theme.href + '" selected="selected">' + theme.href + '</option>';
+                    $link = '<link class="appTheme" rel="stylesheet" href="' + theme.href + '">';
+                }
+                jQuery(this.element).append($option);
+                jQuery(this.element).after($link);
+            }.bind(this));
+
+
+            jQuery(this.element).on('change', function (event) {
+
+                var href = event.target.value;
+
+                jQuery('link.appTheme').each(function (index, elm) {
+                    if (jQuery(elm).attr('href').toLowerCase() === href.toLowerCase()) {
+                        jQuery(elm).attr('disabled', null);
+                    } else {
+                        jQuery(elm).attr('disabled', 'disabled');
+                    }
+                });
+
+            }.bind(this))
         }
-    });
-    Template.themeSwitcher.helpers({
-        themes: function() {
-            return Template.instance().themes.get();
-        }
-    });
-}
+    };
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                    new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
